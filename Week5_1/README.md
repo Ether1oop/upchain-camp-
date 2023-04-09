aad19994df21d40c87efce0f7358816ba6994ba0
Ether1oop <1536622592@qq.com>
2023-04-09 17:57:59 +0800

    update

---
+++ Week5_1/README.md
@@ -0,0 +1,19 @@
+# 使用ChainLink Automation条件调用
+
+
+## 任务地址
+
+
+[https://automation.chain.link/bnb-chain-testnet/63897092515764749308545211177363768392091472120058075075621263593935341964614](https://automation.chain.link/bnb-chain-testnet/63897092515764749308545211177363768392091472120058075075621263593935341964614)
+
+## ERC20地址
+
+[https://testnet.bscscan.com/address/0xEbe8a4970931e091F310f0140b105B70dD761740](https://testnet.bscscan.com/address/0xEbe8a4970931e091F310f0140b105B70dD761740)
+
+## Vault地址
+
+[https://testnet.bscscan.com/address/0xA32cbEA260814e15834451932B8E9f36da48Ad8d](https://testnet.bscscan.com/address/0xA32cbEA260814e15834451932B8E9f36da48Ad8d)
+
+## AutoCollectUpKeep合约地址
+
+[https://testnet.bscscan.com/address/0x850b1fA646aC2e25de62d0F627337c83d8a889Ce](https://testnet.bscscan.com/address/0x850b1fA646aC2e25de62d0F627337c83d8a889Ce)
---
+++ Week5_1/test/Vault.js
@@ -0,0 +1,66 @@
+require("@nomicfoundation/hardhat-network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Etherloop", function () {
+    let Etherloop;
+    let etherloop;
+    let Vault;
+    let vault;
+    let owner;
+
+    beforeEach(async function(){
+        Etherloop = await ethers.getContractFactory("Etherloop");
+        etherloop = await Etherloop.deploy();
+        await etherloop.deployed();
+
+        [owner] = await ethers.getSigners();
+
+        Vault = await ethers.getContractFactory("Vault");
+        vault = await Vault.deploy();
+        await vault.deployed(); 
+    })
+
+
+    describe("deposit by approve",function(){
+        /**
+         * 由owner调用ERC20开始授权，然后Vault调用存款
+         */
+        it("should deposit", async function(){
+            // 进行授权与验证
+            await etherloop.connect(owner).approve(vault.address, 1000);
+            expect(await etherloop.allowance(owner.address, vault.address)).to.be.equal(1000);
+            // 进行存款与验证
+            await vault.connect(owner).deposit(etherloop.address, 1000);
+            expect(await vault.getBalance(etherloop.address,owner.address)).to.be.equal(1000);
+            
+            await vault.HalfDeposit(etherloop.address);
+            var address_ = await vault.owner();
+            console.log(await etherloop.balanceOf(address_));
+
+        });
+    });
+
+    describe("deposit by tokenReceived", async function(){
+        /**
+         * 由owner调用带回调的函数
+         */
+        it("should deposit", async function(){
+            await etherloop.connect(owner).transferWithCallback(vault.address,1000);
+            expect(await vault.getBalance(etherloop.address,owner.address)).to.be.equal(1000);
+        })
+    });
+
+    describe("withdraw", async function(){
+        it("should withdraw successfully",async function(){
+            const _balance = await etherloop.balanceOf(owner.address);
+            await etherloop.connect(owner).transferWithCallback(vault.address,1000);
+            expect(await vault.getBalance(etherloop.address, owner.address)).to.be.equal(1000);
+            expect(await etherloop.balanceOf(owner.address)).to.be.equal(_balance - 1000);
+
+            await vault.connect(owner).withdraw(etherloop.address,1000);
+            expect(await vault.getBalance(etherloop.address, owner.address)).to.be.equal(0);
+            expect(await etherloop.balanceOf(owner.address)).to.be.equal(_balance);
+        })
+    })
+});
